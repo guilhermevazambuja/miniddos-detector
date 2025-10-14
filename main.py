71d19969a493aaedca746233bae289057e68408c
Gui <88752991+guilhermevazambuja@users.noreply.github.com>
2025-10-14 16:57:04 -0300

    Add main pipeline

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from src.ddos_detector_pipeline import run_ddos_detection_pipeline
 from src.download import download_dataset
+from src.eda import run_eda
 
 if __name__ == "__main__":
     download_dataset()
+    df_cleaned, results_df, df_train, train_svd_df, components_df = run_ddos_detection_pipeline()
+    run_eda(df_cleaned, df_train, train_svd_df, components_df)
